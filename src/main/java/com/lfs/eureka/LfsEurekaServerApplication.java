baf2ba9812d4bcebfc8d55899392536d0406d8a9
zhaorongxi <263801911@qq.com>
2020-09-15 16:13:33 +0800

    1.修改包名称 2.修复异常

--- src/main/java/com/lfs/eureka/LfsEurekaServerApplication.java
+++ src/main/java/com/lfs/eureka/LfsEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.bc.eureka;
+package com.lfs.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class BcEurekaServerApplication {
+public class LfsEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BcEurekaServerApplication.class, args);
+		SpringApplication.run(LfsEurekaServerApplication.class, args);
 	}
 
 }
